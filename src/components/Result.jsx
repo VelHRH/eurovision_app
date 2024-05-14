58eb8b227454dd48c0cf3ba96aa326f0df9f083a
Valentine Yurchenko <yrchenko644@gmail.com>
2024-05-14 14:07:18 +0300

    Delete src directory

--- src/components/Result.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Result = ({ res, curWords }) => {
- return (
-  <div className="text-red-500 text-center font-bold text-3xl my-3">
-   Ваш результат: {`${res} / ${curWords.length}`}
-  </div>
- );
-};
