4c561136b074dfde0dca45146d11591d257fd5f9
VelHRH <yrchenko644@gmail.com>
2024-06-18 11:07:02 +0300

    spring server init

---
+++ server/src/main/java/com/example/eurovisionapp/EurovisionappApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurovisionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurovisionappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurovisionappApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/eurovisionapp/EurovisionappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurovisionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurovisionappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
